dcbb3ef05b75216365a75850abc8ae5e2390c61d
cloudinmotion-acc <aaleti.bharath@accenture.com>
2026-02-11 12:19:10 +0000

    modified compose file

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 resource "null_resource" "docker_compose" {
   provisioner "local-exec" {
+    working_dir = path.module
     command = <<EOT
-      POSTGRES_USER=${var.postgres_user} \
-      POSTGRES_PASSWORD=${var.postgres_password} \
-      POSTGRES_DB=${var.postgres_db} \
-      N8N_ENCRYPTION_KEY=${var.n8n_encryption_key} \
-      N8N_USER_MANAGEMENT_JWT_SECRET=${var.n8n_user_management_jwt_secret} \
-      N8N_DEFAULT_BINARY_DATA_MODE=${var.n8n_default_binary_data_mode} \
-      N8N_SECURE_COOKIE="false" \
+      export POSTGRES_USER="${var.postgres_user}"
+      export POSTGRES_PASSWORD="${var.postgres_password}"
+      export POSTGRES_DB="${var.postgres_db}"
+      export N8N_ENCRYPTION_KEY="${var.n8n_encryption_key}"
+      export N8N_USER_MANAGEMENT_JWT_SECRET="${var.n8n_user_management_jwt_secret}"
+      export N8N_DEFAULT_BINARY_DATA_MODE="${var.n8n_default_binary_data_mode}"
+      export N8N_SECURE_COOKIE="false"
       sudo -E docker compose -f ${path.module}/docker-compose.yml up -d
     EOT
-    working_dir = path.module
   }
 
   lifecycle {
@@ -18,8 +18,8 @@ resource "null_resource" "docker_compose" {
   }
 
   provisioner "local-exec" {
-    when    = destroy
-    command = "sudo docker compose -f ${path.module}/docker-compose.yml down"
+    when        = destroy
     working_dir = path.module
+    command     = "sudo docker compose -f ${path.module}/docker-compose.yml down"
   }
 }
